2ff1662a3eac9ba554115e74ddd2559cfa8640c9
Evan Duncan <evan.e.duncan@gmail.com>
2022-08-14 01:31:15 -0500

    Add some fancy stuff

--- app/views/layouts/application.php
+++ app/views/layouts/application.php
@@ -3,7 +3,13 @@
 
 <head>
     <meta charset=utf-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&family=PT+Sans+Narrow&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css">
+    <link rel="stylesheet" href="/css/application.css" type="text/css">
 </head>
 
 <body>
